ae94d05e9c507224a0c078137da8b72307a90de8
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-09-13 20:19:51 +0400

    some'

---
+++ main.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector('.btn')
+let counterArea = document.querySelector('.count-area')
+let count = 0
+
+btn.addEventListener('click', () => {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    let rgb = `rgb(${r}, ${g}, ${b})`
+  document.body.style.backgroundColor = rgb
+    count++
+    counterArea.innerHTML = `ფერი შეიცვალა ${count}-ჯერ`
+    localStorage(count)
+})
